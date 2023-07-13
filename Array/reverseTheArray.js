6612c844d98fd82d01556d3534612b0a30d8f397
iamdeepakram <deepakramsutrakar@gmail.com>
2023-07-13 22:55:15 +0530

    update while condition

--- Array/reverseTheArray.js
+++ Array/reverseTheArray.js
@@ -12,17 +12,12 @@ let arr = [4,5,1,2]
 let start = 0;
 let end = arr.length-1;
 
-while(start < end ){
-
+while(start<end){
     let temp = arr[start];
     arr[start] = arr[end];
     arr[end] = temp;
-    
     start++;
     end--;
-
-   
-
 }
 console.log(arr);
 
